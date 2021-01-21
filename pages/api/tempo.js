d22a04d9b81f3b5caea227bb059a8a4580662d59
marcostrike11 <marcopereira1111@gmai.com>
2021-01-21 00:15:55 +0000

    'apiRefresh'

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,12 @@
-function tempo(request, response){
+async function tempo(request, response){
     const dynamicDate = new Date();
+    //const api = await fetch("teste");
+
+    response.setHeader('Cache-Control', 's-maxage=10, stable-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString()
+        //api: api.toString()
     });
 }
 
